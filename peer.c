4941a7a0ff6cbef659277151167e38e0989a5782
Robert Seaton <seatonr@dupage.edu>
2011-08-09 09:18:52 -0500

    Updated to use linked list, removed unused variable.
    Signed-off-by: Robert Seaton <seatonr@dupage.edu>

--- peer.c
+++ peer.c
@@ -4,16 +4,21 @@
 
 #include "includes.h"
 
-struct Peer* init_peer (struct Torrent* t, char* addr, char* port)
+struct Peer* init_peer (char* addr, char* port)
 {
      struct Peer* p = malloc(sizeof(struct Peer));
-     memcpy(p->addr.sin_addr.s_addr, addr, sizeof(p->addr.sin_addr.s_addr));
-     memcpy(p->addr.sin_port, port, sizeof(p->addr.sin_port));
+     memcpy((void *)&p->addr.sin_addr.s_addr, addr, sizeof(p->addr.sin_addr.s_addr));
+     memcpy((void *)&p->addr.sin_port, port, sizeof(p->addr.sin_port));
      p->state = NOT_CONNECTED | CHOKED;
      p->bitfield = NULL;
      p->addr.sin_family = AF_INET;
      p->amount_downloaded = 0;
      time(&p->started);
+#ifdef DEBUG
+     p->dot_ip = inet_ntoa(p->addr.sin_addr);
+#endif
+
+     return p;
 }
 
 struct PeerNode* init_peer_node (struct Peer* cargo, struct PeerNode* next)
@@ -28,30 +33,36 @@ struct PeerNode* init_peer_node (struct Peer* cargo, struct PeerNode* next)
 void add_peer (struct PeerNode* current, struct Peer* peer)
 {
      /* handle the first peer in the linked list */
-     if (current->cargo->addr.sin_addr.s_addr == peer->addr.sin_addr.s_addr)
+     if (current->cargo->addr.sin_addr.s_addr == peer->addr.sin_addr.s_addr) {
+          
           return ;
-
+     }
      /* check against the rest of the peers in the linked list */
      while (current->next != NULL) {
           current = current->next;
           if (current->cargo->addr.sin_addr.s_addr == peer->addr.sin_addr.s_addr)
                return ;
      } 
-
+     
      /* if we made it this far, that means our peer isn't already contained
       * in the linked list
       * * */
      current->next = init_peer_node(peer, NULL);
 }
 
-void add_peers (struct Torrent* t, struct Peer** peer_list)
+void add_peers (struct Torrent* t, struct PeerNode* peer_list)
 {
      struct PeerNode* current = t->peer_list;
-     uint64_t i = 0;
+   
      /* possible this is called from the first announce */
-     if (current == NULL) 
-          current = init_peer_node(peer_list[i++], NULL);
-     
-     while (peer_list[i])
-          add_peer(current, peer_list[i++]);
+     if (t->peer_list == NULL) {
+          current = init_peer_node(peer_list->cargo, NULL);
+          peer_list = peer_list->next;
+     }
+
+     /* iterate over all the peers */
+     while (peer_list != NULL) {
+          add_peer(current, peer_list->cargo);
+          peer_list = peer_list->next;
+     }
 }
