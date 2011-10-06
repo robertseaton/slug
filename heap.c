7104d14e00d85f45b6104aa437efdbf4adaab463
robertseaton <robb@adbge.org>
2011-10-06 09:19:46 -0500

    priority_queue.c -> heap.c

--- heap.c
+++ heap.c
@@ -1,4 +1,11 @@
+/*
+ * heap.c
+ * implements a min binary heap for use as a priority queue 
+ * (t->pieces, t->downloading)
+ * * */
 #include <stdlib.h>
+#include <string.h>
+
 #include "includes.h"
 
 #define LEFT(x)   (2 * x)
@@ -24,51 +31,67 @@ compare_priority (struct Piece x, struct Piece y)
           return 0;
 }
 
+int8_t
+compare_age (struct Piece x, struct Piece y)
+{
+     if (x.started < y.started)
+          return 1;
+     else
+          return 0;
+}
+
+/* pq = priority queue */
 void 
-heapify (struct MinBinaryHeap* pq, uint64_t i)
+heapify (struct MinBinaryHeap* pq, uint64_t i, int8_t (*compare)(struct Piece, struct Piece))
 {
      uint64_t minimum;
      uint64_t l = LEFT(i);
      uint64_t r = RIGHT(i);
      
      /* check left child node */
-     if (compare_priority(pq->elements[l], pq->elements[i]))
+     if (compare(pq->elements[l], pq->elements[i]))
           minimum = l;
      else
           minimum = i;
 
      /* check right child node */
-     if (compare_priority(pq->elements[r], pq->elements[minimum]))
+     if (compare(pq->elements[r], pq->elements[minimum]))
           minimum = r;
 
      if (minimum != i) {
           swap(pq->elements[i], pq->elements[minimum]);
-          heapify(pq, minimum);
+          heapify(pq, minimum, compare);
      }     
 }
 
 struct Piece* 
 heap_min (struct MinBinaryHeap* pq)
 {
-     return &(pq->elements[1]);
+     if (pq->heap_size > 0)
+          return &(pq->elements[1]);
+     else
+          return NULL;
 }
 
-struct Piece 
-heap_extract_min (struct MinBinaryHeap* pq)
+struct Piece* 
+heap_extract_min (struct MinBinaryHeap* pq, int8_t (*compare)(struct Piece, struct Piece))
 {
-     struct Piece min;
+     struct Piece* min = malloc(sizeof(struct Piece));
      
      if (pq->heap_size >= 1) {
-          min = pq->elements[1];
+          memcpy(min, &(pq->elements[1]), sizeof(struct Piece));
           pq->elements[1] = pq->elements[(pq->heap_size)--];
-          heapify(pq, 1);
+          heapify(pq, 1, compare);
+     } else {
+          free(min);
+          min = NULL;
      }
 
      return min;
 }
 
 int8_t 
-heap_insert (struct MinBinaryHeap* pq, struct Piece key)
+heap_insert (struct MinBinaryHeap* pq, struct Piece key, int8_t (*compare)(struct Piece, struct Piece))
 {
      uint64_t i;
      
@@ -76,7 +99,7 @@ heap_insert (struct MinBinaryHeap* pq, struct Piece key)
           return -1;
 
      i = ++(pq->heap_size);
-     while (i > 1 && compare_priority(key, pq->elements[PARENT(i)])) {
+     while (i > 1 && compare(key, pq->elements[PARENT(i)])) {
           pq->elements[i] = pq->elements[PARENT(i)];
           i = PARENT(i);
      }
@@ -86,14 +109,14 @@ heap_insert (struct MinBinaryHeap* pq, struct Piece key)
 }
 
 int8_t 
-heap_delete (struct MinBinaryHeap* pq, uint64_t i)
+heap_delete (struct MinBinaryHeap* pq, uint64_t i, int8_t (*compare)(struct Piece, struct Piece))
 {
      if (i < pq->heap_size || i < 1)
           return -1;
 
      pq->elements[i] = pq->elements[(pq->heap_size)--];
 
-     heapify(pq, i);
+     heapify(pq, i, compare);
      return 0;
 }
 
@@ -109,10 +132,29 @@ struct Piece*
 find_by_index (struct MinBinaryHeap* pq, uint64_t index)
 {
      uint64_t i;
-     for (i = 0; i < pq->heap_size; i++)
+     for (i = 1; i < pq->heap_size + 1; i++)
           if (pq->elements[i].index == index)
                return &(pq->elements[i]);
 
      /* invalid index */
+     return NULL;
+}
+
+struct Piece*
+extract_by_index (struct MinBinaryHeap* pq, uint64_t index, int8_t (*compare)(struct Piece, struct Piece))
+{
+     struct Piece* out = malloc(sizeof(struct Piece));
+
+     uint64_t i;
+     for (i = 1; i < pq->heap_size + 1; i++)
+          if (pq->elements[i].index == index) {
+               memcpy(out, &(pq->elements[i]), sizeof(struct Piece));
+               pq->elements[i] = pq->elements[(pq->heap_size)--];
+               heapify(pq, i, compare);
+               return out;
+          }
+
+     /* invalid index */
+     free(out);
      return NULL;
 }
