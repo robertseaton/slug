3c7572e0412d49d615cae6f5c08a32be07e7b501
robertseaton <robb@adbge.org>
2011-10-06 09:24:01 -0500

    Added file with functions for operating on linked list of peers (list.c) and added build rules for both list.c and heap.c to the Makefile.

---
+++ list.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+
+#include "includes.h"
+
+struct Peer* 
+extract_element (struct PeerNode** head, struct Peer* peer)
+{
+     /* check if peer is the head */
+     if ((*head)->cargo == peer) {
+          *head = (*head)->next;
+          
+          return peer;
+     }
+
+     struct PeerNode* prev = *head;
+     struct PeerNode* current = (*head)->next;
+
+     while (current != NULL) {
+          if (current->cargo == peer) {
+               prev->next = current->next;
+
+               return peer;
+          }
+          
+          prev = current;
+          current = current->next;
+     }
+
+     /* if we reach this far, peer wasn't in linked list */
+     return NULL;
+}
+
+void
+insert_head (struct PeerNode** head, struct Peer* peer)
+{
+     struct PeerNode* new_head = malloc(sizeof(struct PeerNode));
+
+     new_head->cargo = peer;
+     new_head->next = *head;
+     *head = new_head;
+}
+
+void
+insert_tail (struct PeerNode* list, struct Peer* peer)
+{
+     struct PeerNode* head = list;
+     struct PeerNode* tail = malloc(sizeof(struct PeerNode));
+     tail->cargo = peer;
+     tail->next = NULL;
+
+     while (list->next != NULL)
+          list = list->next;
+
+     list->next = tail;
+     list = head;
+}
